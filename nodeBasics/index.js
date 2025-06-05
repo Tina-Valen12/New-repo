e5ab7a99757eb7a04b51c123128ea1e952399a87
Tina <bahmet.valentyna@gmail.com>
2025-06-05 10:13:09 +0300

    Updated Homework

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -3,4 +3,4 @@ const log = console.log;
 const text = 'Test test test';
 log(chalk.blue('Hello') + ' World' + chalk.red('!'));
 log(chalk.cyan(text));
-log(chalk.red('Red') + chalk.yellow(' Hot') + chalk.redBright(' Chili') + chalk.greenBright(' Peppers'));
+log(chalk.red('Red') + chalk.yellow('Hot') + chalk.redBright('Chili') + chalk.greenBright('Peppers'));
