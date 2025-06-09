e41137424cf3ca43a1b41867902c313277745f1f
= <=>
2025-06-09 22:45:25 +0300

    Homework 5

---
+++ HW5/task3.js
@@ -0,0 +1,12 @@
+const number = 7;
+for (let i = 1; i < 11; i++) {
+  console.log(number + " x " + i + " = " + i * number);
+}
+
+console.log('\n');
+
+let count = 1;
+while (count < 11) {
+    console.log(number + " x " + count + " = " + count * number);
+    count++;
+}
