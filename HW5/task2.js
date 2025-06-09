dc2dce3eab4693078aff59b74ffe5c7ad67951ba
= <=>
2025-06-10 00:27:38 +0300

    Fixed task1, task2

--- HW5/task2.js
+++ HW5/task2.js
@@ -1,18 +1,18 @@
-let averageGrade = 10;
+let averageGrade = 72;
 
 switch (true) {
     case averageGrade <60:
         console.log ("Незадовільно");
         break;
-    case averageGrade >=60 && averageGrade <=70:
+    case averageGrade <=70:
         console.log ("Задовільно");
         break
-    case averageGrade >=71 && averageGrade <=80:
+    case averageGrade <=80:
         console.log ("Добре");
         break
-    case averageGrade >=81 && averageGrade <=90:
+    case averageGrade <=90:
         console.log ("Дуже добре");
         break
-    case averageGrade >=91 && averageGrade <=100:
+    case averageGrade <=100:
         console.log ("Відмінно");
 }
