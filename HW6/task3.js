cf9b6e4528aa0c51feeed3e979d1f552e4e3a829
= <=>
2025-06-16 11:32:41 +0300

    Fixed task3 (v2)

--- HW6/task3.js
+++ HW6/task3.js
@@ -1,16 +1,18 @@
 function checkOrder(available, ordered) {
+
     if(available < ordered) {
         console.log("Your order is too large, we don’t have enough goods.");
         return;
     }
-    else if(ordered == 0) {
+
+    if(ordered === 0) {
         console.log("Your order is empty.");
         return;
     }
-    else if(available >= ordered) {
-        console.log("Your order is accepted.");
-        return;
-    }
-}
 
-checkOrder(10,10);
+    console.log("Your order is accepted.");
+    }
+
+checkOrder(1,5);
+checkOrder(5,0);
+checkOrder(10,5);
