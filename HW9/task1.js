84cbb2b9de6a12f8d7de784d80e435fe8cd2ef72
= <=>
2025-06-23 22:52:25 +0300

    Homework9

---
+++ HW9/task1.js
@@ -0,0 +1,9 @@
+const student = {};
+
+student.firstName = "Ann";
+student.lastName = "Smith";
+student.age = 19;
+student.courses = ["Mathematics", "Chemistry", "History"];
+
+console.log(student);
+
